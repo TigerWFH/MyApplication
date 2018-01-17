bd66fab21e43a49287a84fa8d0601ae39d9981b4
wangfanghua <wangfanghua@jk.cn>
2018-01-17 17:06:39 +0800

    调通bottomnavigation

---
+++ app/src/main/java/com/example/monkey/myapplication/BaseFragment.java
@@ -0,0 +1,38 @@
+package com.example.monkey.myapplication;
+
+/**
+ * Created by wangfanghua on 2018/1/17.
+ */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.support.design.widget.Snackbar;
+
+public class BaseFragment extends Fragment {
+    public static BaseFragment newInstance(String info) {
+        Bundle args = new Bundle();
+        BaseFragment fragment = new BaseFragment();
+        args.putString("info", info);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_base, null);
+        TextView tvInfo = (TextView) view.findViewById(R.id.textView);
+        tvInfo.setText(getArguments().getString("info"));
+        tvInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Don't click me.please!.", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+    }
+}
--- app/src/main/java/com/example/monkey/myapplication/MainActivity.java
+++ app/src/main/java/com/example/monkey/myapplication/MainActivity.java
@@ -5,66 +5,101 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.PagerAdapter;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 import android.app.AlertDialog;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private AlertDialog.Builder builder;
     private Button toDialog;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };
+    private AlertDialog.Builder builder;
+    private ViewPager viewPager;
+    private MenuItem menuItem;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button toWebView = (Button) findViewById(R.id.btn);
-        toWebView.setOnClickListener(this);
-        toDialog = (Button) findViewById(R.id.dialog);
-        toDialog.setOnClickListener(this);
+        viewPager = (ViewPager) findViewById(R.id.viewPage);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener(){
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                        switch (item.getItemId()){
+                            case R.id.navigation_home:
+                                viewPager.setCurrentItem(0);
+                                break;
+                            case R.id.navigation_dashboard:
+                                viewPager.setCurrentItem(1);
+                                break;
+                            case R.id.navigation_monkey:
+                                viewPager.setCurrentItem(2);
+                                break;
+                        }
+
+                        return false;
+                    }
+                });
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener(){
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+            @Override
+            public void onPageSelected(int position) {
+                if (menuItem != null) {
+                    menuItem.setChecked(false);
+                }
+                else {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                menuItem = bottomNavigationView.getMenu().getItem(position);
+                menuItem.setChecked(true);
+            }
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        setupViewPager(viewPager);
+
+//        Button toWebView = (Button) findViewById(R.id.btn);
+//        toWebView.setOnClickListener(this);
+//        toDialog = (Button) findViewById(R.id.dialog);
+//        toDialog.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View view) {
-        switch(view.getId()) {
-            case R.id.dialog:
-                builder = new AlertDialog.Builder(this);
-                builder.setTitle("monkey");
-                builder.setCancelable(true);
-                AlertDialog dg = builder.create();
-                dg.show();
-                break;
-            case R.id.btn:
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, Main2Activity.class);
-                startActivity(intent);
-                break;
-        }
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(BaseFragment.newInstance("home"));
+        adapter.addFragment(BaseFragment.newInstance("dashBord"));
+        adapter.addFragment(BaseFragment.newInstance("monkey"));
+        viewPager.setAdapter(adapter);
     }
+
+
+//    @Override
+//    public void onClick(View view) {
+//        switch(view.getId()) {
+//            case R.id.dialog:
+//                builder = new AlertDialog.Builder(this);
+//                builder.setTitle("monkey");
+//                builder.setCancelable(true);
+//                AlertDialog dg = builder.create();
+//                dg.show();
+//                break;
+//            case R.id.btn:
+//                Intent intent = new Intent();
+//                intent.setClass(MainActivity.this, Main2Activity.class);
+//                startActivity(intent);
+//                break;
+//        }
+//    }
 }
