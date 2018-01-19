f8dfea40a16926afbb725ac89ec801ca13cd9e12
wangfanghua <wangfanghua@jk.cn>
2018-01-19 19:56:54 +0800

    webView一些设置

--- app/src/main/java/com/example/monkey/myapplication/MonkeyFragment.java
+++ app/src/main/java/com/example/monkey/myapplication/MonkeyFragment.java
@@ -4,20 +4,20 @@ package com.example.monkey.myapplication;
  * Created by wangfanghua on 2018/1/18.
  */
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebViewClient;
-import android.widget.TextView;
-import android.support.design.widget.Snackbar;
 import android.webkit.WebView;
 
 public class MonkeyFragment extends Fragment {
+    private WebView webView;
     public static MonkeyFragment newInstance(String info) {
         Bundle args = new Bundle();
         MonkeyFragment fragment = new MonkeyFragment();
@@ -30,14 +30,29 @@ public class MonkeyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.monkey_view, null);
-        WebView webView = view.findViewById(R.id.mkWebView);
-        webView.loadUrl("https://www.baidu.com");
+        webView = view.findViewById(R.id.mkWebView);
         webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                System.out.println("***********begin loading***********");
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                System.out.println("***********end loading***********");
+                super.onPageFinished(view, url);
+            }
+
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
                 return super.shouldOverrideUrlLoading(view, request);
             }
         });
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.loadUrl("https://www.baidu.com");
         return view;
     }
 }
